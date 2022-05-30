e5d442197e3ad75d01d69910db29034ee4795612
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-05-30 01:27:53 +0000

    [Finish]応用課題7aいいね順に表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    @books = Book.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
     @book = Book.new
   end
 
